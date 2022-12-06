e8265a39673bec8d0eb6b75377c1cf67e01e337c
Zach <zach.hill@me.com>
2022-12-06 07:15:04 +0000

    Scoreboard reads in scores from a db file now

--- flask_app/static/javascript/leaderboard.js
+++ flask_app/static/javascript/leaderboard.js
@@ -1,5 +1,18 @@
-windows.onload = filltable;
+window.onload = filltable;
 
-filltable(){
-    
+function filltable(){
+    const tablevals = score
+    const table = document.getElementById('maintable')
+    for(i = 0; i < tablevals.length; i++){
+        console.log(tablevals[i])
+        let row = table.insertRow();
+        let rank = row.insertCell();
+        let ranktext = document.createTextNode(i + 1);
+        rank.appendChild(ranktext);
+        for(key in tablevals[i]){
+            let cell = row.insertCell();
+            let text = document.createTextNode(tablevals[i][key])
+            cell.appendChild(text);
+        }
+    }
 }

dfc2544a57d55be5bba037cf57e1662ca583bac6
Zach <zach.hill@me.com>
2022-12-06 07:17:10 +0000

    Scoreboard reads from db file

--- flask_app/static/javascript/leaderboard.js
+++ flask_app/static/javascript/leaderboard.js
@@ -1,8 +1,10 @@
 window.onload = filltable;
 
 function filltable(){
+    //get the values from the database file
     const tablevals = score
     const table = document.getElementById('maintable')
+    //iterate through each row adding a value to rank, name, and time
     for(i = 0; i < tablevals.length; i++){
         console.log(tablevals[i])
         let row = table.insertRow();

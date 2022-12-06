c1b003d351e4ce2a1f7dfc88fceb66a524d7271b
Zach <zach.hill@me.com>
2022-12-06 06:46:44 +0000

    Test db to use for scoreboard

---
+++ flask_app/testscore.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+c.execute('CREATE TABLE score(name VARCHAR(45), time int);')
+
+c.execute("INSERT INTO score VALUES('Frank', 60);")
+c.execute("INSERT INTO score VALUES('Jack', 75);")
+c.execute("INSERT INTO score VALUES('Jim', 45);")
+c.execute("INSERT INTO score VALUES('Karen', 15);")
+c.execute("INSERT INTO score VALUES('Jam,es', 30);")
+
+conn.commit()
+conn.close()

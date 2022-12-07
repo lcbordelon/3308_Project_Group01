a338a663ec1610b62279bdc1f8530630e245f345
Zach <zach.hill@me.com>
2022-12-07 01:08:47 +0000

    fixed initscore

--- flask_app/init_scoredb.py
+++ flask_app/init_scoredb.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 #adds the score table to the database.db file
 import sqlite3
 
 connection = sqlite3.connect('database.db')
 c = connection.cursor()
 
-c.exectute("CREATE TABLE score(name VARCHAR(20), time INT);")
+c.execute("CREATE TABLE score(name VARCHAR(20), time INT);")
 
-conn.commit()
-conn.close()
+connection.commit()
+connection.close()
